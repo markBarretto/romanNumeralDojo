1285971dae171981e56b3bf1f42bf51b719bfb0e
mark.barretto <mark.barretto@gmail.com>
2014-12-11 15:18:24 +0800

    end of session 1

--- jasmine/spec/romanNumeralsSpec.js
+++ jasmine/spec/romanNumeralsSpec.js
@@ -9,9 +9,40 @@ describe("Roman Numerals", function() {
 	romanNumeral = new RomanNumerals();
   });
 
-  it("Character I should equal to 1", function() {
+  it("Number 1 should equal to Character I", function() {
     //return value
-    expect(romanNumeral.convert(1)).toBe('I');
+    var output = romanNumeral.convert(1);
+    
+    expect(output).toBe("I");
   });
-
+  
+  it("Number 2 should equal to II", function() {
+    var output = romanNumeral.convert(2);
+    
+    expect(output).toBe("II");
+  });
+  
+  it("Number 3 should equal to III", function() {
+  	var output = romanNumeral.convert(3);
+  	
+  	expect(output).toBe("III");
+  
+  });
+  
+  it("Number 5 should equal to V", function(){
+  	var output = romanNumeral.convert(5);
+  	
+  	expect(output).toBe('V');
+  })
+  
+  it("Number 8 should equal to VIII", function(){
+  	var output = romanNumeral.convert(8);
+  	expect(output).toBe('VIII');
+  });
+  
+  it("Number 4 should equal to IV", function(){
+  	var output = romanNumeral.convert(4);
+  	expect(output).toBe('IV');
+  });
+  
 });
--- jasmine/src/romanNumerals.js
+++ jasmine/src/romanNumerals.js
@@ -1,2 +1,26 @@
-function RomanNumerals() {
+var RomanNumerals = function() {
 }
+
+
+RomanNumerals.prototype.convert = function(input) {
+	var result = "";
+	var counter = input;
+	if (counter>=5)
+	{ 		
+		result = "V";
+	
+		counter -=5;
+	}
+	
+	
+	
+	
+	for (var i=1; i<=counter; i++ )
+	{
+		result = result + "I";
+	}
+	
+	return result;
+}
+
+
